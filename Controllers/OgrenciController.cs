26ad923606c12f01471229ee289dbc0c88be9bb6
gkadirgil <64523795+gkadirgil@users.noreply.github.com>
2021-01-04 13:40:54 +0300

    first commit

---
+++ Controllers/OgrenciController.cs
@@ -0,0 +1,37 @@
+﻿using createPartialView.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace createPartialView.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            Ogrenci student = new Ogrenci();
+            student.Name = "Gokhan";
+            student.SurName = "Gulsen";
+            student.Age = 28;
+
+            return View(student);
+        }
+        [HttpGet]
+        public IActionResult YeniOgrenci()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult YeniOgrenci(Ogrenci student)
+        {
+            Ogrenci alinanOgrenci = new Ogrenci();
+            alinanOgrenci.Name = student.Name;
+            alinanOgrenci.SurName = student.SurName;
+            alinanOgrenci.Age = student.Age;
+
+            return View(alinanOgrenci);
+        }
+    }
+}
---
+++ Models/Ogrenci.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace createPartialView.Models
+{
+    public class Ogrenci
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public byte Age { get; set; }
+    }
+}
